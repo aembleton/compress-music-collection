191989249bbf9d988907f0db7c3e8b1ac2294b86
Arthur Embleton <arthur.embleton@bbc.co.uk>
2014-08-26 16:32:12 +0100

    Improved the startup message

--- src/main/scala/Run.scala
+++ src/main/scala/Run.scala
@@ -5,7 +5,12 @@ object Run {
   def main (args: Array[String]): Unit = {
     //read in the args and start up akka
     if (args.length != 3) {
-      println("You must enter")
+      println(
+        """You must enter the following three arguments:
+          | - Directory from which to read from (eg ~/Music)
+          | - Directory to create the new, smaller collection (eg ~/small-music)
+          | - Maximum size of the new collection in GB (eg 32)
+        """.stripMargin)
       System.exit(1)
     }
   }
