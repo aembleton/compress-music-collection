8eea8ffdee1b1153abeffe656f90f3a5956a6da8
Arthur Embleton <arthur.embleton@bbc.co.uk>
2014-08-26 10:20:48 +0100

    A basic check on args length

--- build.sbt
+++ build.sbt
@@ -1,4 +1,3 @@
 name := "compress-music-collection"
 
 version := "1.0"
-    
---
+++ src/main/scala/Run.scala
@@ -0,0 +1,12 @@
+
+
+object Run {
+
+  def main (args: Array[String]): Unit = {
+    //read in the args and start up akka
+    if (args.length != 3) {
+      println("You must enter")
+      System.exit(1)
+    }
+  }
+}
